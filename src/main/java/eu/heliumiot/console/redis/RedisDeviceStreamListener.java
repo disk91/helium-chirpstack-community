48d203aba0114b614b82e6e6a1d8cb891f8b5a70
Paul <disk_91@hotmail.com>
2022-12-21 14:42:19 +0100

    manage stream not existing on redis

--- src/main/java/eu/heliumiot/console/redis/RedisDeviceStreamListener.java
+++ src/main/java/eu/heliumiot/console/redis/RedisDeviceStreamListener.java
@@ -39,6 +39,11 @@ public class RedisDeviceStreamListener {
     protected int runningJobs;
     protected boolean serviceEnable; // false to stop the services
 
+    // When the Chripstack has just been created, it seems we can have the stream not yet
+    // existing and so we fail in creating the consumer group and fetch data
+    // so we will monitor this on the running job.
+    protected boolean streamExists = false; // true if the groups has been created correctly aka stream not existing
+    protected long lastStreamTry = 0;
 
     @PostConstruct
     public void setupRedisStreamMetaListener() {
@@ -52,22 +57,6 @@ public class RedisDeviceStreamListener {
         RedisCodec<String, byte[]> codec = RedisCodec.of(new StringCodec(), new ByteArrayCodec());
         redisConnection = redisClient.connect(codec);
         syncCommands = redisConnection.sync();
-
-        try {
-            syncCommands.xgroupCreate(
-                    XReadArgs.StreamOffset.from(
-                            redisConfiguration.getStreamMetaKey(),
-                            "0-0"),
-                    redisConfiguration.getRedisCGroup()
-            );
-        } catch (RedisBusyException redisBusyException) {
-            // consumer group exist ... basically a normal situation
-        } catch (Exception x) {
-            // the stream does not exist yet ... until the first device it seems we don't have it or it's name
-            // has changed ... not sure ...
-            // by the way, skip it for now
-            log.error("### TODO Manage the stream question ...");
-        }
 
         this.runningJobs = 0;
         this.serviceEnable = true;
@@ -95,23 +84,44 @@ public class RedisDeviceStreamListener {
         this.runningJobs++;
         long start = Now.NowUtcMs();
         int processed = 0;
-        try {
 
-            @SuppressWarnings("unchecked")
-            List<StreamMessage<String, byte[]>> messages = syncCommands.xreadgroup(
-                    Consumer.from(redisConfiguration.getRedisCGroup(), redisConfiguration.getRedisConsumer()),
-                    XReadArgs.StreamOffset.lastConsumed(redisConfiguration.getStreamMetaKey())
-            );
+        // Let's create the group if possible / needed, no need to check faster than once a minute
+        if ( ! this.streamExists && ( Now.NowUtcMs() - this.lastStreamTry ) > 60_000  ) {
+            this.lastStreamTry = Now.NowUtcMs();
+            try {
+                syncCommands.xgroupCreate(
+                        XReadArgs.StreamOffset.from(
+                                redisConfiguration.getStreamMetaKey(),
+                                "0-0"),
+                        redisConfiguration.getRedisCGroup()
+                );
+                this.streamExists = true;
+            } catch (RedisBusyException redisBusyException) {
+                // consumer group exist ... basically a normal situation
+            } catch (Exception x) {
+                // the stream does not exist yet ... until the first device it seems we don't have it or it's name
+            }
+            if ( !this.streamExists ) log.warn("Impossible to create the redis stream reader, will try later");
+        }
 
-            if (!messages.isEmpty()) {
-                for (StreamMessage<String, byte[]> message : messages) {
-                    Map<String, byte[]> m = message.getBody();
-                    for (String k : m.keySet()) {
-                        processed++;
-                        if (k.compareToIgnoreCase("up") == 0) {
-                            // @Todo - see how REDIS can be use later
-                            // Redis would be the right way to get information but the payload size
-                            // is not secured here. MQTT message are safer
+        if ( this.streamExists ) {
+            try {
+
+                @SuppressWarnings("unchecked")
+                List<StreamMessage<String, byte[]>> messages = syncCommands.xreadgroup(
+                        Consumer.from(redisConfiguration.getRedisCGroup(), redisConfiguration.getRedisConsumer()),
+                        XReadArgs.StreamOffset.lastConsumed(redisConfiguration.getStreamMetaKey())
+                );
+
+                if (!messages.isEmpty()) {
+                    for (StreamMessage<String, byte[]> message : messages) {
+                        Map<String, byte[]> m = message.getBody();
+                        for (String k : m.keySet()) {
+                            processed++;
+                            if (k.compareToIgnoreCase("up") == 0) {
+                                // @Todo - see how REDIS can be use later
+                                // Redis would be the right way to get information but the payload size
+                                // is not secured here. MQTT message are safer
                             /*
                             log.debug("REDIS - Receiving uplink");
                             byte[] byteData = message.getBody().get(k);
@@ -135,46 +145,46 @@ public class RedisDeviceStreamListener {
                                 log.info(HexaConverters.byteToHexStringWithSpace(byteData));
                             }
                             */
-                        } else if (k.compareToIgnoreCase("down") == 0) {
-                            log.debug("REDIS - Receiving downlink/ack/join accept");
-                            byte[] byteData = message.getBody().get(k);
-                            try {
-                                DownlinkFrameLog dwn = DownlinkFrameLog.parseFrom(byteData);
-                                if (dwn != null) {
-                                    int downlinkSize = dwn.getPhyPayload().size();
-                                    if (downlinkSize <= 12) downlinkSize = 0;
-                                    else downlinkSize -= 13;
+                            } else if (k.compareToIgnoreCase("down") == 0) {
+                                log.debug("REDIS - Receiving downlink/ack/join accept");
+                                byte[] byteData = message.getBody().get(k);
+                                try {
+                                    DownlinkFrameLog dwn = DownlinkFrameLog.parseFrom(byteData);
+                                    if (dwn != null) {
+                                        int downlinkSize = dwn.getPhyPayload().size();
+                                        if (downlinkSize <= 12) downlinkSize = 0;
+                                        else downlinkSize -= 13;
 
-                                    log.debug("Dev: " + dwn.getDevEui() + " Adr:" + dwn.getDevAddr() + " size: " + downlinkSize);
-                                    heliumTenantService.processDownlink(
-                                            null,
-                                            dwn.getDevEui(),
-                                            downlinkSize
-                                    );
+                                        log.debug("Dev: " + dwn.getDevEui() + " Adr:" + dwn.getDevAddr() + " size: " + downlinkSize);
+                                        heliumTenantService.processDownlink(
+                                                null,
+                                                dwn.getDevEui(),
+                                                downlinkSize
+                                        );
+                                    }
+                                } catch (InvalidProtocolBufferException x) {
+                                    log.error("Impossible to parse stream type up with " + x.getMessage());
+                                    log.info(HexaConverters.byteToHexStringWithSpace(byteData));
                                 }
-                            } catch (InvalidProtocolBufferException x) {
-                                log.error("Impossible to parse stream type up with " + x.getMessage());
+                            } else {
+                                log.warn("## Found a new key on stream:meta " + k);
+                                byte[] byteData = message.getBody().get(k);
                                 log.info(HexaConverters.byteToHexStringWithSpace(byteData));
                             }
-                        } else {
-                            log.warn("## Found a new key on stream:meta " + k);
-                            byte[] byteData = message.getBody().get(k);
-                            log.info(HexaConverters.byteToHexStringWithSpace(byteData));
                         }
+                        // Confirm that the message has been processed using XACK
+                        syncCommands.xack(
+                                redisConfiguration.getStreamMetaKey(),
+                                redisConfiguration.getRedisCGroup(),
+                                message.getId()
+                        );
                     }
-                    // Confirm that the message has been processed using XACK
-                    syncCommands.xack(
-                            redisConfiguration.getStreamMetaKey(),
-                            redisConfiguration.getRedisCGroup(),
-                            message.getId()
-                    );
                 }
+            } catch (RedisCommandExecutionException x) {
+                log.error("### Stream not existing, we should not be here ");
+            } finally {
+                this.runningJobs--;
             }
-        } catch ( RedisCommandExecutionException x ) {
-            // @TODO what happen
-            log.error("### TODO fix this ... the stream is not existing ");
-        } finally {
-          this.runningJobs--;
         }
         log.debug("ListenOnRedisStreamMeta - duration "+(Now.NowUtcMs()-start)+" ms, process "+processed+" new entries");
 
