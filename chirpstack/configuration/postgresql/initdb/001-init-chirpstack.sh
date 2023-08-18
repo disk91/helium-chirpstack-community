b81fad923e74d26b6900d21565ac1eea496be5cb
Paul <disk_91@hotmail.com>
2023-08-18 17:43:22 +0200

    making config file ready for scriting

--- chirpstack/configuration/postgresql/initdb/001-init-chirpstack.sh
+++ chirpstack/configuration/postgresql/initdb/001-init-chirpstack.sh
@@ -2,6 +2,6 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create role chirpstack with login password 'fdfce449c0ba34332525';
+    create role chirpstack with login password '@DBPASSWORD@';
     create database chirpstack with owner chirpstack;
 EOSQL
