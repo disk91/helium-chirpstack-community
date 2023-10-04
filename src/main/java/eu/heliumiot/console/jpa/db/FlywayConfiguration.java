cc868624cc04d30bbc43f6c05841349bb9170b9a
Paul <disk_91@hotmail.com>
2023-10-04 23:02:47 +0200

    make sure JPA is before FlyWay

--- src/main/java/eu/heliumiot/console/jpa/db/FlywayConfiguration.java
+++ src/main/java/eu/heliumiot/console/jpa/db/FlywayConfiguration.java
@@ -3,10 +3,12 @@ package eu.heliumiot.console.jpa.db;
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 import javax.sql.DataSource;
 
 @Configuration
+@DependsOn("entityManagerFactory")
 public class FlywayConfiguration {
 
     // Manually start flyway to have it starting once JPA job is finished
