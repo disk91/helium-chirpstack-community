ba54a71797338b92fdbdbe518b0ee312122cb2f0
Paul <disk_91@hotmail.com>
2023-12-02 20:00:03 +0100

    better support of MQTT broker disconnection for internal communication

--- src/main/java/eu/heliumiot/console/mqtt/MqttSender.java
+++ src/main/java/eu/heliumiot/console/mqtt/MqttSender.java
@@ -19,26 +19,11 @@
  */
 package eu.heliumiot.console.mqtt;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import eu.heliumiot.console.ConsoleConfig;
-import eu.heliumiot.console.jpa.repository.TenantRepository;
-import eu.heliumiot.console.mqtt.api.HeliumDeviceActDeactItf;
-import eu.heliumiot.console.mqtt.api.HeliumDeviceStatItf;
-import eu.heliumiot.console.service.HeliumDeviceStatService;
-import eu.heliumiot.console.service.HeliumTenantService;
 import eu.heliumiot.console.service.PrometeusService;
-import fr.ingeniousthings.tools.DateConverters;
-import fr.ingeniousthings.tools.HexaConverters;
-import fr.ingeniousthings.tools.Now;
-import fr.ingeniousthings.tools.RandomString;
-import io.chirpstack.json.JoinEvent;
-import io.chirpstack.json.UplinkEvent;
+import fr.ingeniousthings.tools.*;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-import org.postgresql.shaded.com.ongres.scram.common.bouncycastle.base64.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +31,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 
 @Component
 public class MqttSender implements MqttCallback {
@@ -61,88 +47,120 @@ public class MqttSender implements MqttCallback {
     protected PrometeusService prometeusService;
 
     private MqttConnectOptions connectionOptions;
-        private MemoryPersistence persistence;
-        private MqttClient mqttClient;
+    private MemoryPersistence persistence;
+    private MqttClient mqttClient;
+    private String clientId;
 
-        @PostConstruct
-        public MqttClient initMqtt() {
+    protected boolean requestToStop = false;
+    protected volatile int scheduleRunning = 0;
+    protected boolean stopped = false;
 
-                String clientId = mqttConfig.getMqttId() + RandomString.getRandomString(4);
-                this.persistence = new MemoryPersistence();
-                this.connectionOptions = new MqttConnectOptions();
-                try {
-                        log.info("MQTT S Url :" + mqttConfig.getMqttServer());
-                        log.info("MQTT S User :" + mqttConfig.getMqttLogin());
-                        //log.info("Password :"+mqttConfig.getPassword());
-                        log.info("MQTT S Id : "+clientId);
-                        this.mqttClient = new MqttClient(mqttConfig.getMqttServer(), clientId, persistence);
-                        this.connectionOptions.setCleanSession(true);
-                        this.connectionOptions.setAutomaticReconnect(false); // managed manually
-                        this.connectionOptions.setConnectionTimeout(5);
-                        this.connectionOptions.setKeepAliveInterval(60);
-                        this.connectionOptions.setUserName(mqttConfig.getMqttLogin());
-                        this.connectionOptions.setPassword(mqttConfig.getMqttPassword().toCharArray());
-                        this.connect();
-                        log.info("MQTT S Starting Mqtt listener");
-                } catch (MqttException me) {
-                        log.error("MQTT S ERROR : "+me.getMessage());
-                }
-                return this.mqttClient;
+    public void stopMqttListener() {
+        this.requestToStop = true;
+        this.stop();
+        long start = Now.NowUtcMs();
+        while ( this.scheduleRunning > 0 && (Now.NowUtcMs() - start) < 10_000 ) {
+            Tools.sleep(100);
         }
+        this.stopped = true;
+    }
 
-        public void connect() throws MqttException {
-                log.debug("MQTT S - Connect");
-                this.mqttClient.connect(this.connectionOptions);
-                this.mqttClient.setCallback(this);
+    @PostConstruct
+    public MqttClient initMqtt() {
+
+        this.clientId = mqttConfig.getMqttId() + RandomString.getRandomString(4);
+        this.persistence = new MemoryPersistence();
+        this.connectionOptions = new MqttConnectOptions();
+        try {
+            log.info("MQTT S Url :" + mqttConfig.getMqttServer());
+            log.info("MQTT S User :" + mqttConfig.getMqttLogin());
+            //log.info("Password :"+mqttConfig.getPassword());
+            log.info("MQTT S Id : "+clientId);
+            this.connectionOptions.setCleanSession(true);
+            this.connectionOptions.setAutomaticReconnect(false); // managed manually
+            this.connectionOptions.setConnectionTimeout(5);
+            this.connectionOptions.setKeepAliveInterval(60);
+            this.connectionOptions.setUserName(mqttConfig.getMqttLogin());
+            this.connectionOptions.setPassword(mqttConfig.getMqttPassword().toCharArray());
+            this.connect();
+            log.info("MQTT S Starting Mqtt listener");
+        } catch (MqttException me) {
+            log.error("MQTT S ERROR : "+me.getMessage());
         }
+        return this.mqttClient;
+    }
 
-        public void stop() {
-                try {
-                        this.mqttClient.disconnect();
-                        this.mqttClient.close();
-                } catch (MqttException me) {
-                        log.error("MQTT S ERROR :"+me.getMessage());
-                }
+    private boolean inConnect = false;
+    public void connect() throws MqttException {
+        inConnect = true;
+        try {
+            log.debug("MQTT S - Connect");
+            if (this.mqttClient != null) this.mqttClient.close();
+            this.mqttClient = new MqttClient(mqttConfig.getMqttServer(), this.clientId, persistence);
+            this.mqttClient.connect(this.connectionOptions);
+            this.mqttClient.setCallback(this);
+        } finally {
+            inConnect = false;
         }
+    }
 
 
-        private boolean pendingReconnection = false;
+    public void stop() {
+        try {
+            this.mqttClient.disconnect();
+            this.mqttClient.disconnect();
+            this.mqttClient.close();
+        } catch (MqttException me) {
+            log.error("MQTT S ERROR :"+me.getMessage());
+        }
+    }
 
-        @Override
-        public void connectionLost(Throwable arg0) {
-            log.error("MQTT S - Connection Lost");
-            try {
-                // immediate retry, then will be async
-                log.error("MQTT S - Direct reconnecting");
+
+    private boolean pendingReconnection = false;
+
+    @Override
+    public void connectionLost(Throwable arg0) {
+        log.error("MQTT S - Connection Lost");
+        try {
+            // immediate retry, then will be async
+            log.error("MQTT S - Direct reconnecting");
+            this.connect();
+            log.error("MQTT S - Direct reconnected");
+            this.sendPending();
+        } catch (MqttException me) {
+            log.warn("MQTT S - direct reconnection failed - "+me.getMessage());
+            pendingReconnection = true;
+        }
+        prometeusService.addMqttConnectionLoss();
+    }
+
+    @Scheduled(fixedDelayString = "${helium.mqtt.reconnect.scanPeriod}", initialDelay = 30_000) // default 10s
+    protected void autoReconnect() {
+        if ( ! pendingReconnection || inConnect ) return;
+        try {
+            if ( mqttClient.isConnected() ) {
+                log.info("MQTT S - reconnected");
+            } else {
+                log.info("MQTT S - reconnecting");
                 this.connect();
-                log.error("MQTT S - Direct reconnected");
-            } catch (MqttException me) {
-                log.warn("MQTT S - direct reconnection failed - "+me.getMessage());
-                pendingReconnection = true;
+                log.info("MQTT S - reconnected");
+                this.sendPending();
             }
-            prometeusService.addMqttConnectionLoss();
+            pendingReconnection = false;
+        } catch (MqttException me) {
+            log.warn("MQTT S - reconnection failed - "+me.getMessage());
         }
+    }
 
-        @Scheduled(fixedDelayString = "${helium.mqtt.reconnect.scanPeriod}", initialDelay = 30_000) // default 10s
-        protected void autoReconnect() {
-            if ( ! pendingReconnection ) return;
-            try {
-                if ( mqttClient.isConnected() ) {
-                    log.info("MQTT S - reconnected");
-                } else {
-                    log.info("MQTT S - reconnecting");
-                    this.connect();
-                    log.info("MQTT S - reconnected");
-                }
-                pendingReconnection = false;
-            } catch (MqttException me) {
-                log.warn("MQTT S - reconnection failed - "+me.getMessage());
-            }
-        }
+    protected static class MqttBackupMessage {
+        public String topic;
+        public String message;
+        public int qos;
+    }
 
-
-        public void publishMessage(String topic, String message, int qos) {
-
+    ArrayList<MqttBackupMessage> messageBackupQueue = new ArrayList<>();
+    public void publishMessage(String topic, String message, int qos) {
+        if ( this.mqttClient.isConnected() ) {
             try {
                 MqttMessage mqttmessage = new MqttMessage(message.getBytes());
                 mqttmessage.setQos(qos);
@@ -151,22 +169,43 @@ public class MqttSender implements MqttCallback {
             } catch (MqttException me) {
                 log.error("MQTT S - MessagePublish Error", me);
             }
-
+        } else {
+            // buffer the message for later
+            if ( messageBackupQueue.size() < 1000 ) {
+                MqttBackupMessage m = new MqttBackupMessage();
+                m.topic = topic;
+                m.message = message;
+                m.qos = qos;
+                messageBackupQueue.add(m);
+                log.info("MQTT S - Delayed messages for later as broker is disconnected");
+            }
         }
+    }
 
-        /*
-         * (non-Javadoc)
-         * @see org.eclipse.paho.client.mqttv3.MqttCallback#deliveryComplete(org.eclipse.paho.client.mqttv3.IMqttDeliveryToken)
-         */
-        @Override
-        public void deliveryComplete(IMqttDeliveryToken arg0) {
-                // log.info("MQTT - delivery completed");
-
+    protected void sendPending() {
+        // publish all the pending messages
+        if (this.mqttClient.isConnected() && messageBackupQueue != null && messageBackupQueue.size() > 0) {
+            ArrayList<MqttBackupMessage> toSend = new ArrayList<>(messageBackupQueue);
+            messageBackupQueue.clear();
+            for ( MqttBackupMessage m : toSend ) {
+                publishMessage(m.topic,m.message,m.qos);
+            }
         }
+    }
 
-        @Override
-        public void messageArrived(String topicName, MqttMessage message) throws Exception {
+    /*
+     * (non-Javadoc)
+     * @see org.eclipse.paho.client.mqttv3.MqttCallback#deliveryComplete(org.eclipse.paho.client.mqttv3.IMqttDeliveryToken)
+     */
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken arg0) {
+        // log.info("MQTT - delivery completed");
 
-        }
+    }
+
+    @Override
+    public void messageArrived(String topicName, MqttMessage message) throws Exception {
+
+    }
 
 }
