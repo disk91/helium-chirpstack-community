c6c8d5ec461c01df1df4389fc136cb04b1d8c530
Paul <disk_91@hotmail.com>
2023-12-20 21:26:42 +0100

    authorize + in email

--- src/main/java/fr/ingeniousthings/tools/Tools.java
+++ src/main/java/fr/ingeniousthings/tools/Tools.java
@@ -13,7 +13,7 @@ public class Tools {
     }
 
     public static boolean isValidEmailSyntax(String email) {
-        return email.matches("^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+        return email.matches("^(?=.{1,64}@)[A-Za-z0-9_+-]+(\\.[A-Za-z0-9_-]+)*@"
                 + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$");
     }
 
