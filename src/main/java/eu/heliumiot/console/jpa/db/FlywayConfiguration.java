2410193d77cbe6720ae034d61e51254fbbdf7841
Paul <disk_91@hotmail.com>
2024-01-08 22:20:08 +0100

    Common - add postgresql dep in flyway

--- src/main/java/eu/heliumiot/console/jpa/db/FlywayConfiguration.java
+++ src/main/java/eu/heliumiot/console/jpa/db/FlywayConfiguration.java
@@ -1,6 +1,8 @@
 package eu.heliumiot.console.jpa.db;
 
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -11,10 +13,18 @@ import javax.sql.DataSource;
 @DependsOn("entityManagerFactory")
 public class FlywayConfiguration {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     // Manually start flyway to have it starting once JPA job is finished
     @Autowired
     public FlywayConfiguration(DataSource dataSource) {
-        Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+        log.info("Running Flaywaydb ...");
+        try {
+            Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 }
