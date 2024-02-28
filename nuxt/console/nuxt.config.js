25c8b5b3154bd7d13300d7a3751c5a3c349f8f81
Paul <disk_91@hotmail.com>
2024-02-28 16:26:13 +0100

    prepare next version

--- nuxt/console/nuxt.config.js
+++ nuxt/console/nuxt.config.js
@@ -78,7 +78,7 @@ export default {
     disableStatTenant:process.env.DIS_STAT_TENANT || 'false',
     community:process.env.DIS_FULL || 'true',
     mapApiKey:process.env.MAP_KEY || '',
-    frontVersion:'1.8.1'
+    frontVersion:'1.9.0'
   },
 
   // env variables loaded accessible on the server side
